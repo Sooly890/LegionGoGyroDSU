2bf25efb9574dc87582bdc1d6c5e7851cf6f16da
Sooly890 <solomontnz@gmail.com>
2026-02-22 11:17:37 +1300

    minor comment

--- scripts/install.sh
+++ scripts/install.sh
@@ -6,7 +6,7 @@ sudo systemctl stop lgsdsu.service
 sudo systemctl disable lgsdsu.service
 sudo rm /etc/systemd/system/lgsdsu.service
 sudo systemctl daemon-reload
-sudo rm /etc/modprobe.d/fix-iio.conf
+sudo rm /etc/modprobe.d/fix-iio.conf # old, but in case anybody installed it from the era
 sudo rm /etc/modules-load.d/fix-iio-sensor-hub.conf
 
 sudo rm -rf /LegionGoSGyroDSU
