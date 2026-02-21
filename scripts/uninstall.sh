c21a5a97c87535fca03e2a12d1c111ddb7931ecf
Sooly890 <solomontnz@gmail.com>
2026-02-22 11:16:36 +1300

    Fix uninstall.sh

--- scripts/uninstall.sh
+++ scripts/uninstall.sh
@@ -4,5 +4,7 @@ sudo systemctl stop lgsdsu.service
 sudo systemctl disable lgsdsu.service
 sudo rm /etc/systemd/system/lgsdsu.service
 sudo systemctl daemon-reload
+sudo rm /etc/modprobe.d/fix-iio.conf
+sudo rm /etc/modules-load.d/fix-iio-sensor-hub.conf
 
 sudo rm -rf /LegionGoSGyroDSU
