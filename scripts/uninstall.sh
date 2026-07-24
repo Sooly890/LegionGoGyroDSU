2eacaeeaf25d60845d15f6e908e4faae67a9f19f
Sooly <130508235+Sooly890@users.noreply.github.com>
2026-07-24 17:08:47 +1200

    Update uninstall.sh for rename

--- scripts/uninstall.sh
+++ scripts/uninstall.sh
@@ -2,9 +2,16 @@
 
 sudo systemctl stop lgsdsu.service
 sudo systemctl disable lgsdsu.service
-sudo rm /etc/systemd/system/lgsdsu.service
+sudo rm -f /etc/systemd/system/lgsdsu.service
+
+sudo systemctl stop lgdsu.service
+sudo systemctl disable lgdsu.service
+sudo rm -f /etc/systemd/system/lgdsu.service
+
 sudo systemctl daemon-reload
-sudo rm /etc/modprobe.d/fix-iio.conf
-sudo rm /etc/modules-load.d/fix-iio-sensor-hub.conf
+
+sudo rm -f /etc/modprobe.d/fix-iio.conf
+sudo rm -f /etc/modules-load.d/fix-iio-sensor-hub.conf
 
 sudo rm -rf /LegionGoSGyroDSU
+sudo rm -rf /LegionGoGyroDSU
